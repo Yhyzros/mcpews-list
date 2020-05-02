1ab100d5d7e05fdf2921708fe0c0f532381315fe
三海伊月 <63846667+Yhyzros@users.noreply.github.com>
2020-05-02 14:57:21 +0800

    js

--- wsFrameTmp.js
+++ wsFrameTmp.js
@@ -1,13 +1,12 @@
-const WebSocket = require('ws');
-const crypto = require('crypto');
-const fs = require('fs');
-const uuid = () =>([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>(c ^ (crypto.randomBytes(1)[0] & (15 >> (c / 4)))).toString(16));
-const ip = '127.0.0.1';
+const WebSocket = require('ws');//需要使用npm i ws进行安装
+const crypto = require('crypto');//依赖项
+const fs = require('fs');//依赖项
+const uuid = () =>([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>(c ^ (crypto.randomBytes(1)[0] & (15 >> (c / 4)))).toString(16));//uuid
 const wss = new WebSocket.Server({ port: 17439 });
 //End for it.
-console.log('Server is running at '+ ip + ':17439.');
+console.log('Server is running at 127.0.0.1:17439.');//服务器运行在本地端口.
 console.log('Author by Yhyzros.');
-//subscribe.
+
 wss.on('connection', (ws) =>{
     ws.send(JSON.stringify({
         body: {
@@ -15,7 +14,7 @@ wss.on('connection', (ws) =>{
         },
         header: buildHeader('subscribe')
     }));
-    //Definition:command
+    //定义command↓
     function command(cmd){
         ws.send(JSON.stringify({
             body:{
@@ -28,27 +27,30 @@ wss.on('connection', (ws) =>{
             header: buildHeader('commandRequest')
         }));
     }
+//定义结束
+    //msg↓
     ws.on("message",(msg) =>{
-        console.log('onMessgae:', msg):
+        console.log('onMessgae:', msg);
         if(JSON.parse(msg).body.eventName == "PlayerMessage"){
-            let say = JSON.parse(msg).body.properties.Message;//msg
-            let str = message.split(' ');//split
+            /*设定玩家触发词*/let say = JSON.parse(msg).body.properties.Message;//msg
+            /*截取玩家触发词后的字符串*/let str = message.split(' ');//split
             let sender = JSON.parse(msg).body.properties.Sender;//sender
-            let CarryCommand = 'Null';
+            /*定义cmd功能执行*/let CarryCommand = 'Null';
             //console.log(`${sender} says ${say}`);
-    if(say == 'PlayerMessageTest'){
-        command('say Tested!!');
+    /*功能测试触发词(say)*/if(say == 'PlayerMessageTest'){
+        /*发送命令*/command('say Tested!!');
     };
 
-    if(str == 'exec '){
-        CarryCommand = msg.substring(5,1000);
-        command(CarryCommand);
-        command('say command:' + CarryCommand);
+    /*执行功能命令*/if(str == 'exec '){
+        /*设为截取触发词后的命令*/CarryCommand = msg.substring(5,1000);
+        /*输出命令*/command(CarryCommand);
+        /*告诉玩家输出的命令*/command('say command:' + CarryCommand);
     }
         }
     })
 });//Server end.
 
+   /*消息返回到uuid库↓*/
    function buildHeader(purpose){
         return{
             version: 1,
@@ -57,3 +59,4 @@ wss.on('connection', (ws) =>{
             messageType:'commandRequest'
         }
    }
+//End
